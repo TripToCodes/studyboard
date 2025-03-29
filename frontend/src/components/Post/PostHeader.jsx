57f560202fd41003b39dcbc2aad30c7a4a785417
TripToCodes <iam.somj@gmail.com>
2025-03-29 01:55:20 -0700

    Display author and date in the post page by correct props, improve css

--- frontend/src/components/Post/PostHeader.jsx
+++ frontend/src/components/Post/PostHeader.jsx
@@ -1,24 +1,26 @@
 import "../../styles/PostHeader.css";
 import PropTypes from "prop-types";
 
-const PostHeader = ({ title, author, date }) => {
+const PostHeader = ({ post }) => {
   return (
     <div id="post-header">
-      <div className="post-wrapper">
-        <h1 className="post-header-title">{title}</h1>
-        <div className="post-header-info">
-          <span className="post-header-author">{author}</span>
-          <span className="post-header-date">{date}</span>
-        </div>
+      <div className="post-header-title-wrapper">
+        <h1 className="post-header-title">{post.title}</h1>
+      </div>
+      <div className="post-header-info-wrapper">
+        <span className="post-header-author">{post.author}</span>
+        <span className="post-header-date">{post.date}</span>
       </div>
     </div>
   );
 };
 
 PostHeader.propTypes = {
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string,
-  date: PropTypes.string,
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default PostHeader;
